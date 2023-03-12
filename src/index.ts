f52cea33be44540bec62d52253b77c72188645d9
CommanderRedYT <ccommanderred@gmail.com>
2023-03-12 16:42:57 +0100

    Added pushover

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { ClickHouse } from 'clickhouse';
 import dotenv from 'dotenv';
+//@ts-ignore
+import PushOver from 'pushover-notifications';
+
+let lastIsOpen = false;
 
 // configure environment variables
 dotenv.config();
@@ -11,6 +15,11 @@ const clickhouse = new ClickHouse({
         username: process.env.CLICKHOUSE_USERNAME || 'default',
         password: process.env.CLICKHOUSE_PASSWORD || '',
     }
+});
+
+const pushover = new PushOver({
+    user: process.env.PUSHOVER_USER_KEY,
+    token: process.env.PUSHOVER_API_TOKEN,
 });
 
 const url = 'https://realraum.at/status.json';
@@ -35,7 +44,25 @@ export async function insertData(data: object): Promise<void> {
 export default function main(): void {
     // execute every 5 seconds
     setInterval(async () => {
-        const data = await getData();
+        const data = await getData() as { open: boolean, [key: string]: any };
+
+        if (data.hasOwnProperty('open') && data.open !== lastIsOpen) {
+            lastIsOpen = data.open;
+
+            const message = {
+                message: `Realraum is ${data.open ? 'open' : 'closed'} now`,
+                title: 'Realraum',
+                device: 'Samsung-S21',
+                priority: 1,
+            };
+
+            pushover.send(message, (error: any, _result: any) => {
+                if (error) {
+                    console.log(error);
+                }
+            });
+        }
+
         await insertData(data);
     }, 5000);
 
